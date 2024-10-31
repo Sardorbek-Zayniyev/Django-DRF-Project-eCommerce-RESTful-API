f5be9f6be138f647d1f95b0b409a3c5457072ece
Sardorbek-Zayniyev <me.zayniyev@gmail.com>
2024-11-01 03:17:04 +0500

    🆕 feat: create category, brand, and product tables

--- drfecommerce/product/models.py
+++ drfecommerce/product/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    parent = TreeForeignKey(
+        "self", on_delete=models.PROTECT, null=True, blank=True)
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Product(models.Model):
+    category = TreeForeignKey(
+        'Category', on_delete=models.SET_NULL, null=True, blank=True)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    is_digital = models.BooleanField(default=False)
