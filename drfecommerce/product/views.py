bb1243b4b5b1847283b702e31c0de60c37b30cd6
Sardorbek-Zayniyev <me.zayniyev@gmail.com>
2024-11-03 01:43:56 +0500

    🔍 feat: add category-based product filtering

--- drfecommerce/product/views.py
+++ drfecommerce/product/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from drf_spectacular.utils import extend_schema
 from .models import Category, Brand, Product
 from .serializers import CategorySerializer, BrandSerializer, ProductSerializer
@@ -18,6 +19,7 @@ class CategoryViewSet(viewsets.ViewSet):
         serializer = CategorySerializer(self.queryset, many=True)
         return Response(serializer.data)
 
+
 class BrandViewSet(viewsets.ViewSet):
     """
     A simple Viewset for viewing all brands
@@ -30,6 +32,7 @@ class BrandViewSet(viewsets.ViewSet):
         serializer = BrandSerializer(self.queryset, many=True)
         return Response(serializer.data)
 
+
 class ProductViewSet(viewsets.ViewSet):
     """
     A simple Viewset for viewing all products
@@ -40,4 +43,13 @@ class ProductViewSet(viewsets.ViewSet):
     @extend_schema(responses=ProductSerializer)
     def list(self, request):
         serializer = ProductSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+    """
+    An endpoint to return product by category
+    """
+    @action(methods=['get'], detail=False, url_path=r"category/(?P<category>\w+)/all",)
+    def list_product_by_category(self, request, category=None):
+        serializer = ProductSerializer(
+            self.queryset.filter(category__name=category), many=True)
         return Response(serializer.data)
