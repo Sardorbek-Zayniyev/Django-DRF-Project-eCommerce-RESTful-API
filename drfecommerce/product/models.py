587f80994b0067d5c14f17e7cfa9c05d68e9eb8e
Sardorbek-Zayniyev <me.zayniyev@gmail.com>
2024-11-10 20:42:35 +0500

    📝 feat: add custom ordered list field

--- drfecommerce/product/models.py
+++ drfecommerce/product/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
-
+from .fields import OrderField
+from django.core.exceptions import ValidationError
 # Create your models here.
 
 
@@ -45,8 +46,8 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=255)
     description = models.TextField(blank=True)
-    is_digital = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
+    is_digital = models.BooleanField(default=False)
 
     objects = ActiveQueryset.as_manager()
 
@@ -61,6 +62,20 @@ class ProductLine(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=5)
     sku = models.CharField(max_length=100)
     stock_qty = models.IntegerField()
+    order = OrderField(unique_for_field="product", blank=True)
     is_active = models.BooleanField(default=False)
 
     objects = ActiveQueryset.as_manager()
+
+    def clean(self):
+        qs = ProductLine.objects.filter(product=self.product)
+        for obj in qs:
+            if self.id != obj.id and self.order == obj.order:
+                raise ValidationError("Duplicate value.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super(ProductLine, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return str(self.sku)
