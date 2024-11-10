290b3d0ba94ef8b88a3b0161e058d4aa23578ff3
Sardorbek-Zayniyev <me.zayniyev@gmail.com>
2024-11-10 05:19:30 +0500

    🔄 feat: implement serializer field name mapping and flattening

--- drfecommerce/product/serializers.py
+++ drfecommerce/product/serializers.py
@@ -4,28 +4,33 @@ from .models import Category, Brand, Product, ProductLine
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source='name')
+
     class Meta:
         model = Category
-        exclude = ('id',)
+        fields = ['category_name']
 
 
 class BrandSerializer(serializers.ModelSerializer):
+    brand_name = serializers.CharField(source='name')
+
     class Meta:
         model = Brand
-        exclude = ('id',)
+        fields = ['brand_name']
 
 
 class ProductLineSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductLine
-        exclude = ('id',)
+        exclude = ('id', 'product', 'is_active')
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    category = BrandSerializer()
-    brand = BrandSerializer()
+    brand_name = serializers.CharField(source='brand.name')
+    category_name = serializers.CharField(source='category.name')
     product_line = ProductLineSerializer(many=True)
 
     class Meta:
         model = Product
-        exclude = ('id',)
+        fields = ('name', 'slug', 'description',
+                  'brand_name', 'category_name', 'product_line')
