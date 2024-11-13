dac11f656fc6da1010c6d80878b23a952910db16
Sardorbek-Zayniyev <me.zayniyev@gmail.com>
2024-11-13 07:57:07 +0500

    👤 feat: add ProductType inline to admin site

--- drfecommerce/product/models.py
+++ drfecommerce/product/models.py
@@ -150,6 +150,14 @@ class ProductImage(models.Model):
 
 class ProductType(models.Model):
     name = models.CharField(max_length=100)
+    attribute = models.ManyToManyField(
+        Attribute,
+        through="ProductTypeAttribute",
+        related_name="product_type_attribute",
+    )
+
+    def __str__(self):
+        return str(self.name)
 
 
 class ProductTypeAttribute(models.Model):
